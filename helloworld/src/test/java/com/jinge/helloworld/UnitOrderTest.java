88e9ad166ec696075669547f9c12496f8f0fe91e
eltonjiang <eltonjiang@tencent.com>
2018-01-24 19:15:44 +0800

    fix UnitOrderTest

--- helloworld/src/test/java/com/jinge/helloworld/UnitOrderTest.java
+++ helloworld/src/test/java/com/jinge/helloworld/UnitOrderTest.java
@@ -2,26 +2,30 @@ package com.jinge.helloworld;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 /**
  * Sample to show unit execute order.
- * For {@link Before}s, reverse alphabet order.
- * For {@link Test}s and {@link After}, alphabet order.
+ * Method execute order is not predictable. {@link FixMethodOrder}
+ * According <a href="https://github.com/junit-team/junit4/wiki/test-execution-order">test-execution-order</a>,
+ * From version 4.11, JUnit will by default use a deterministic,
+ * but not predictable, order {@link MethodSorters#DEFAULT}.
  * <p>
  * Output of this example is :
  * <pre>
- * CBA
+ * ACB
  * test1
- * output is 3
+ * output is 1
  *
- * CBA
+ * ACB
  * test2
- * output is 4
+ * output is 2
  *
- * CBA
+ * ACB
  * test3
- * output is 5
+ * output is 3
  * </pre>
  */
 
@@ -29,34 +33,37 @@ public class UnitOrderTest {
     private int num = 0;
 
     @Before
-    public void a() {
-        System.out.println("A");
+    public void acc() {
+        System.out.print("A");
     }
 
     @Before
-    public void c() {
+    public void ca() {
         System.out.print("C");
     }
 
     @Before
-    public void b() {
+    public void bb() {
         System.out.print("B");
     }
 
     @Test
     public void test1() throws InterruptedException {
+        System.out.println();
         System.out.println("test1");
         num += 1;
     }
 
     @Test
     public void test3() {
+        System.out.println();
         System.out.println("test3");
         num += 3;
     }
 
     @Test
     public void test2() throws InterruptedException {
+        System.out.println();
         System.out.println("test2");
         Thread.sleep(3000);
         num += 2;

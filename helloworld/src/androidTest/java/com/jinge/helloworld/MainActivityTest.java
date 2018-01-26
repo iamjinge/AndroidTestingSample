7a4a0359ff30c07122e1a29e8a243b43b45c0f1e
eltonjiang <eltonjiang@tencent.com>
2018-01-26 10:50:00 +0800

    add ui thread test

--- helloworld/src/androidTest/java/com/jinge/helloworld/MainActivityTest.java
+++ helloworld/src/androidTest/java/com/jinge/helloworld/MainActivityTest.java
@@ -1,12 +1,17 @@
 package com.jinge.helloworld;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.support.test.internal.runner.junit4.statement.UiThreadStatement;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
 import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -14,12 +19,14 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 /**
- * Created by Elton on 2018/1/25.
+ * Android Unit Tests for {@link MainActivity}
  */
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> activityRule =
@@ -48,5 +55,67 @@ public class MainActivityTest {
         onView(equalTo((View) textView))
                 .perform(click())
                 .check(matches(not(isDisplayed())));
+    }
+
+    /**
+     * The default Test Thread is not UI Thread.
+     * Use {@link ActivityTestRule#runOnUiThread(Runnable)}
+     * of {@link UiThreadStatement#runOnUiThread(Runnable)}
+     * for UI operations.
+     * These methods ensure the Runnable run to the end.
+     * Test Thread will not continue before the Runnable end.
+     * <p>
+     * Output is :
+     * <pre>
+     * waiting
+     * end UI Thread
+     * end
+     * </pre>
+     */
+    @Test
+    public void testUiThread() throws Throwable {
+        System.out.println("waiting");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("end UI Thread");
+            }
+        });
+        System.out.println("end");
+    }
+
+    /**
+     * If you use a custom {@link Handler} to post ui operations.
+     * You should use lock to ensure the execution order.
+     * This trick can also be useful when you run on other Threads.
+     */
+    @Test
+    public void testCustomUiThread() throws InterruptedException {
+        final Object mutex = new Object();
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+        System.out.println("waiting");
+        mainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (mutex) {
+                    mutex.notify();
+                }
+                System.out.println("end UI Thread");
+            }
+        });
+        synchronized (mutex) {
+            mutex.wait();
+        }
+        System.out.println("end");
     }
 }
